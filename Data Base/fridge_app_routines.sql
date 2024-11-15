67a10fded42d7a799545fbcc7e2213d63cbfa335
Maria-Eliza Gozman-Pop <gozmanpopmaria@gmail.com>
2024-11-15 14:47:51 +0200

    mockup commit

--- Data Base/fridge_app_routines.sql
+++ Data Base/fridge_app_routines.sql
@@ -1,5 +1,5 @@
-CREATE DATABASE  IF NOT EXISTS `fridge_app` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `fridge_app`;
+--CREATE DATABASE  IF NOT EXISTS `fridge_app` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+--USE `fridge_app`;
 -- MySQL dump 10.13  Distrib 8.0.36, for Win64 (x86_64)
 --
 -- Host: localhost    Database: fridge_app
